5b02831c25512db936f97460b2ff3befce65e123
janjanjan312 <1262325196@qq.com>
2024-11-06 19:01:36 +0000

    1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2019_09_25_193522) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_06_184946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,14 @@ ActiveRecord::Schema[7.1].define(version: 2019_09_25_193522) do
     t.date "dob"
     t.text "bio"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "link_url"
+    t.string "link_description"
+    t.string "thumbnail_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
